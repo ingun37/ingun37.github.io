be2afad144b06e31876ad4382be2a4a953c978f5
ingun <ingun37@gmail.com>
2019-05-12 00:26:26 +0900

    fullpage

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import logo from './logo.svg';
 import './App.scss';
 import { Cell, Grid, Row } from '@material/react-layout-grid';
 import MyCard from "./MyCard";
-import MyHeader from "./MyHeader";
 import MyGraph from "./MyGraph";
 
 function App() {
   return (
     <Grid>
       <Row>
-        <Cell columns={12}><MyHeader /></Cell>
+        {/* <Cell columns={12}><MyHeader /></Cell> */}
       </Row>
       <Row>
         <Cell columns={3}><MyCard /></Cell>
--- src/MyHeader.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './MyHeader.scss';
-function MyHeader() {
-    return (
-      <h1 className="MyHeader">개발자 전인건입돠</h1>
-    );
-  }
-export default MyHeader;

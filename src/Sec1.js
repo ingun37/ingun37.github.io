6a50a08636cdc8a2fe107f96f937d59045e56790
ingun <ingun37@gmail.com>
2019-05-12 14:33:13 +0900

    sec1

--- src/Sec1.js
+++ src/Sec1.js
@@ -8,12 +8,13 @@ import Card, {
     CardActionButtons,
     CardActionIcons
 } from "@material/react-card";
+import {Image, Video, Transformation, CloudinaryContext} from 'cloudinary-react';
 
 class MyGrid extends React.Component {
     renderCell(width, color, img, title, desc) {
         return (
             <Cell columns={width}>
-                <Card className={`MyCard ${color}`}>
+                <Card className={`MyCard ${color}`} outlined>
                     <CardMedia className="MyCardMedia" imageUrl={process.env.PUBLIC_URL + `/${img}.svg`}>
                         <div className="MediaDiv">
                             <h2>{title}</h2>
@@ -33,12 +34,9 @@ class MyGrid extends React.Component {
 
             <Grid>
                 <Row>
-                    <Cell columns={12}><h1 id="title">WHO AM I?</h1></Cell>
-                </Row>
-                <Row>
                     {this.renderCell(4, 'green', "bridge", "Global", "전 세계 개발자들과의 활발한 소통")}
                     {this.renderCell(4, 'pink', "glasses", "Good Hipster", "신뢰할 수 있는 앞서가는 정보력")}
-                    {this.renderCell(4, 'blue', "horse", "Quick", "병목 없는 생각과 행동")}
+                    {this.renderCell(4, 'blue', "horse", "Quick", "빠르고 정확한 판단과 행동")}
                     {this.renderCell(6, 'blue', "runner", "Discipline", "어려운것, 모르는것은 지금 당장 공부해서 치워야하는 똥")}
                     {this.renderCell(6, 'pink', "sword", "Young Jedi", "12년의 경험: 꼬마시절부터 걷기시작한 개발자의 길")}
                 </Row>
@@ -49,15 +47,16 @@ class MyGrid extends React.Component {
 }
 function MyBG() {
     return (
-      <div className="MyBG"></div>
+      <div className="MyBGDiv">
+        <Image className="MyBG" cloudName="ingun37" publicId="20170904_105517" width={window.innerWidth} crop="scale" />
+      </div>
     )
   }
 class Sec1 extends React.Component {
     render() {
         return (
-            <div>
-            <MyBG/>
-            <MyGrid />
+            <div className="Sec1">
+                <MyGrid />
             </div>
         );
     }

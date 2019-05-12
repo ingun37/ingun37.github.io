bbe5b841d7c4639e0b8ca38cec3057154f4eda79
ingun <ingun37@gmail.com>
2019-05-12 23:14:54 +0900

    lil refactoring

--- src/index.js
+++ src/index.js
@@ -7,27 +7,42 @@ import * as serviceWorker from './serviceWorker';
 import ReactFullpage from '@fullpage/react-fullpage';
 import Sec1 from "./Sec1";
 import posed from "react-pose";
+const deg180 = 180
+const deg0 = 0
+const dayColor = "rgb(218, 199, 30)"
+const nightColor = "rgb(11, 16, 77)"
 
+const SkyDiv = posed.div({
+  [dayColor]: { backgroundColor: dayColor },
+  [nightColor]: { backgroundColor: nightColor },
+})
 
 const RotatingDiv = posed.div({
-  rotated: { rotate: 180, transition: { type: 'spring', ease: 'easeInOut', duration: 1000 } },
-  unrotated: { rotate: 0, transition: { type: 'spring', ease: 'easeInOut', duration: 1000 } }
+  [deg0.toString()]: { rotate: deg0, transition: { type: 'spring', ease: 'easeInOut', duration: 1000 } },
+  [deg180.toString()]: { rotate: deg180, transition: { type: 'spring', ease: 'easeInOut', duration: 1000 } }
 });
 
 class App extends React.Component {
-  state = {isRotated : false}
+  state = { cityDeg: deg0, skyColor: dayColor}
   onLeave(origin, destination, direction) {
     console.log('onLeave', { origin, destination, direction });
-    this.setState({isRotated : !this.state.isRotated})
+    if (origin.index == 0 && destination.index == 1) {
+      this.setState({ cityDeg: deg180,  skyColor: nightColor  })
+    } else if (origin.index == 1 && destination.index == 0) {
+      this.setState({ cityDeg: deg0, skyColor: dayColor })
+    }
+
   }
 
   render() {
     const citysvgURL = 'url(' + process.env.PUBLIC_URL + '/city.svg' + ')'
 
     return (
-      <div className="App">
-        <RotatingDiv className="RotatingDiv MyBG" pose={this.state.isRotated ? "rotated" : "unrotated"} style={{ "maskImage": citysvgURL, "WebkitMaskImage": citysvgURL }}>
-        </RotatingDiv>
+      <SkyDiv className="App" pose={this.state.skyColor} >
+        <RotatingDiv className="RotatingDiv MyBG"
+          pose={this.state.cityDeg.toString()} 
+          style={{ "maskImage": citysvgURL, "WebkitMaskImage": citysvgURL }}
+          />
         <ReactFullpage
           onLeave={this.onLeave.bind(this)}
 
@@ -44,7 +59,7 @@ class App extends React.Component {
             );
           }}
         />
-      </div>
+      </SkyDiv>
     );
   }
 }

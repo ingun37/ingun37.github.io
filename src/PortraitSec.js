e27244f54389f493037781baa7e47c7abed31f2c
ingun <ingun37@gmail.com>
2019-05-13 13:43:57 +0900

    no redundant codes

--- src/PortraitSec.js
+++ src/PortraitSec.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Cell, Grid, Row } from '@material/react-layout-grid';
 import "./PortraitSec.scss"
 import posed from "react-pose";
-import { Z_BINARY } from 'zlib';
 const deg15 = 15
 const deg0 = 0
 
@@ -10,27 +8,6 @@ const RotatingDiv = posed.div({
     [deg0.toString()]: { rotate: deg0, transition: { ease: 'linear', duration: 0 } },
     [deg15.toString()]: { rotate: deg15, transition: { ease: 'linear', duration: 0 } }
 });
-
-class PortraitGrid extends React.Component {
-    render() {
-        return (
-            <Grid className="PortraitGrid">
-                <Row>
-                    <Cell columns="12">
-                    </Cell>
-                </Row>
-                <Row>
-                    <Cell columns="12">
-                    </Cell>
-                </Row>
-                <Row>
-                    <Cell columns="12">
-                    </Cell>
-                </Row>
-            </Grid>
-        );
-    }
-}
 
 class PortraitSec extends React.Component {
     state = { deg: 0 }

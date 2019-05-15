a37eefc6c8d6b3c4ee48b2100d3177b08dab8f2b
ingun <ingun37@gmail.com>
2019-05-15 20:57:44 +0900

    smaller card when mobile

--- src/FeaturesSec.js
+++ src/FeaturesSec.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Cell, Grid, Row } from '@material/react-layout-grid';
-import "./FeaturesSec.scss"
+import { default as scss } from "./FeaturesSec.scss"
 import "./contentpanel.scss"
 import "./cards.scss"
 import "./font.scss"
+import withSizes from 'react-sizes'
 
 import Card, {
     CardPrimaryContent,
@@ -14,36 +15,69 @@ import Card, {
 } from "@material/react-card";
 import { Image, Video, Transformation, CloudinaryContext } from 'cloudinary-react';
 
-class MyGrid extends React.Component {
-    renderCell(width, color, img, title, desc) {
-        return (
-            <Cell columns={width} tabletColumns={12} phoneColumns={12}>
-                <Card className={`MyCard roundcard ${color}`} >
-                    <CardMedia imageUrl={process.env.PUBLIC_URL + `/${img}.svg`} style={{height:"100%"}}>
-                        <div className="blackDiv">
-                            <div className='sectionh2 head'>{title}</div>
-                            <div className='mdc-typography--caption sub'>{desc}</div>
-                        </div>
-                    </CardMedia>
-                </Card>
-            </Cell>
-        );
+function DesktopCell(props) {
+    const {
+        width, color, img, title, desc
+    } = props;
+    return (
+        <Cell columns={width} tabletColumns={12} phoneColumns={12}>
+            <Card className={`MyCard DesktopCard roundcard ${color}`} >
+                <CardMedia imageUrl={process.env.PUBLIC_URL + `/${img}.svg`} style={{ height: "100%" }}>
+                    <div className="blackDiv">
+                        <div className='sectionh2 head'>{title}</div>
+                        <div className='mdc-typography--caption sub'>{desc}</div>
+                    </div>
+                </CardMedia>
+            </Card>
+        </Cell>
+    )
+}
+
+function MobileCell(props) {
+    const {
+        color, img, title, desc
+    } = props;
+    return (
+        <Cell columns={12} tabletColumns={12} phoneColumns={12}>
+            <Card className={`MyCard MobileCard roundcard ${color}`} >
+                <CardMedia imageUrl={process.env.PUBLIC_URL + `/${img}.svg`} style={{ height: "100%" }}>
+                    <div className="blackDiv">
+                        <div className='sectionh2 head'>{title}</div>
+                        <div className='mdc-typography--caption sub'>{desc}</div>
+                    </div>
+                </CardMedia>
+            </Card>
+        </Cell>
+    )
+}
+const desktopBreakpoint = parseInt(scss.desktopbp.match(/^\d+/g).shift() || '840')
+
+const mapSizesToProps = ({ width }) => ({
+    isDesktop: width >= desktopBreakpoint,
+})
+
+function CondCell(props) {
+    if (props.isDesktop) {
+        return DesktopCell(props)
+    } else {
+        return MobileCell(props)
     }
-    render() {
-        return (
-            <div className="MyGrid">
-                <Grid>
-                    <Row>
-                        {this.renderCell(4, 'greencard', "bridge", "Global", "전 세계 개발자들과의 활발한 소통")}
-                        {this.renderCell(4, 'pinkcard', "glasses", "Good Hipster", "신뢰할 수 있는 앞서가는 정보력")}
-                        {this.renderCell(4, 'bluecard', "flash", "Quick", "언제나 빠른 이해, 생각, 행동")}
-                        {this.renderCell(6, 'bluecard', "runner", "Discipline", "어려운것, 모르는것은 지금 당장 공부해서 치워야하는 똥")}
-                        {this.renderCell(6, 'pinkcard', "saber", "Young Jedi", "12년의 경험: 꼬마시절부터 걷기시작한 개발자의 길")}
-                    </Row>
-                </Grid>
-            </div>
-        );
-    }
+}
+
+const MyCell = withSizes(mapSizesToProps)(CondCell)
+
+function MyGrid() {
+    return (
+        <Grid className='featuresgrid'>
+            <Row>
+                <MyCell width={4} color='greencard' img='bridge' title='Global' desc='전 세계 개발자들과의 활발한 소통' />
+                <MyCell width={4} color='pinkcard' img='glasses' title='Good Hipster' desc='신뢰할 수 있는 앞서가는 정보력' />
+                <MyCell width={4} color='bluecard' img='flash' title='Quick' desc='뭐든 언제나 빠른 이해, 생각, 행동' />
+                <MyCell width={6} color='bluecard' img='runner' title='Discipline' desc='어려운것, 모르는것은 지금 당장 공부해서 치워야하는 똥' />
+                <MyCell width={6} color='pinkcard' img='saber' title='Young Jedi' desc='12년의 경험: 꼬마시절부터 걷기시작한 개발자의 길' />
+            </Row>
+        </Grid>
+    );
 }
 
 class FeaturesSec extends React.Component {

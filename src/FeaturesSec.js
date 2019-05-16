ccbc55294db2c849937b1b2de630494836cf07d9
ingun <ingun37@gmail.com>
2019-05-16 21:49:15 +0900

    reword

--- src/FeaturesSec.js
+++ src/FeaturesSec.js
@@ -71,8 +71,8 @@ function MyGrid() {
         <Grid className='featuresgrid'>
             <Row>
                 <MyCell width={4} color='greencard' img='bridge' title='Global' desc='전 세계 개발자들과의 활발한 소통' />
-                <MyCell width={4} color='pinkcard' img='glasses' title='Good Hipster' desc='신뢰할 수 있는 앞서가는 정보력' />
-                <MyCell width={4} color='bluecard' img='flash' title='Quick' desc='뭐든 언제나 빠른 이해, 생각, 행동' />
+                <MyCell width={4} color='pinkcard' img='glasses' title='Good Hipster' desc='앞서가는 정보력' />
+                <MyCell width={4} color='bluecard' img='flash' title='Quick' desc='빠른 이해, 생각, 행동' />
                 <MyCell width={6} color='bluecard' img='runner' title='Discipline' desc='어려운것, 모르는것은 지금 당장 공부해서 치워야하는 똥' />
                 <MyCell width={6} color='pinkcard' img='saber' title='Young Jedi' desc='12년의 경험: 꼬마시절부터 걷기시작한 개발자의 길' />
             </Row>

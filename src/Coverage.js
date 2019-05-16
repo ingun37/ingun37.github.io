680c8d8d1499582a37c4e1ba1829cb7747ff166b
ingun <ingun37@gmail.com>
2019-05-16 16:58:03 +0900

    sigma color export

--- src/Coverage.js
+++ src/Coverage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./Coverage.scss"
+import { default as scss } from "./Coverage.scss"
 import "./contentpanel.scss"
 import "./font.scss"
 import { Sigma, RandomizeNodePositions, RelativeSize, LoadJSON } from 'react-sigma';
@@ -8,8 +8,8 @@ class Coverage extends React.Component {
     render() {
         const setting = {
             labelThreshold: 0,
-            defaultNodeColor: 'rgb(233, 229, 16)',
-            defaultLabelColor: 'rgb(236, 194, 210)',
+            defaultNodeColor: scss.nodecolor,
+            defaultLabelColor: scss.labelcolor,
             mouseEnabled: false,
             touchEnabled: false,
             sideMargin: 0.6,
@@ -19,7 +19,7 @@ class Coverage extends React.Component {
             <div className="Coverage">
                 <h1 className="sectiontitle">COVERAGE</h1>
                 <div className="contentpanel coveragepanel">
-                    <Sigma settings={setting} >
+                    <Sigma  settings={setting} >
                         <LoadJSON path={process.env.PUBLIC_URL + "/graph-data.json"} />
                     </Sigma>
                 </div>
